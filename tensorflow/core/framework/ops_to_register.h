74735dbac83439ac5a21ad729737036ac6c9e10d
Daniel Treiman <dtreiman@alivecor.com>
2017-09-13 22:52:43 -0700

    Got stripped down version that works with hrmutil.

--- tensorflow/core/framework/ops_to_register.h
+++ tensorflow/core/framework/ops_to_register.h
@@ -25,22 +25,27 @@ return ( 0 == str )? NULL : ( true == strcmpc( str, substr ) )? str : strstrc( s
 constexpr inline bool ShouldRegisterOp(const char op[]) {
   return false
      || (strcmpc(op, "Add") == 0)
+     || (strcmpc(op, "BiasAdd") == 0)
      || (strcmpc(op, "Concat") == 0)
      || (strcmpc(op, "Const") == 0)
      || (strcmpc(op, "Conv2D") == 0)
      || (strcmpc(op, "Div") == 0)
      || (strcmpc(op, "Enter") == 0)
      || (strcmpc(op, "Exit") == 0)
+     || (strcmpc(op, "Exp") == 0)
      || (strcmpc(op, "Fill") == 0)
      || (strcmpc(op, "Floor") == 0)
      || (strcmpc(op, "Identity") == 0)
      || (strcmpc(op, "Less") == 0)
+     || (strcmpc(op, "Log") == 0)
      || (strcmpc(op, "LoopCond") == 0)
      || (strcmpc(op, "MatMul") == 0)
+     || (strcmpc(op, "Maximum") == 0)
      || (strcmpc(op, "MaxPool") == 0)
      || (strcmpc(op, "Mean") == 0)
      || (strcmpc(op, "Merge") == 0)
      || (strcmpc(op, "Mul") == 0)
+     || (strcmpc(op, "Neg") == 0)
      || (strcmpc(op, "NextIteration") == 0)
      || (strcmpc(op, "NoOp") == 0)
      || (strcmpc(op, "OneHot") == 0)
@@ -50,14 +55,18 @@ constexpr inline bool ShouldRegisterOp(const char op[]) {
      || (strcmpc(op, "Range") == 0)
      || (strcmpc(op, "Relu") == 0)
      || (strcmpc(op, "Reshape") == 0)
+     || (strcmpc(op, "Rsqrt") == 0)
      || (strcmpc(op, "Shape") == 0)
      || (strcmpc(op, "Sigmoid") == 0)
      || (strcmpc(op, "Slice") == 0)
      || (strcmpc(op, "Softmax") == 0)
      || (strcmpc(op, "SoftmaxCrossEntropyWithLogits") == 0)
      || (strcmpc(op, "Split") == 0)
+     || (strcmpc(op, "Square") == 0)
+     || (strcmpc(op, "Sqrt") == 0)
      || (strcmpc(op, "StridedSlice") == 0)
      || (strcmpc(op, "Sub") == 0)
+     || (strcmpc(op, "Sum") == 0)
      || (strcmpc(op, "Switch") == 0)
      || (strcmpc(op, "Tanh") == 0)
      || (strcmpc(op, "TensorArrayGatherV2") == 0)
@@ -74,10 +83,12 @@ constexpr inline bool ShouldRegisterOp(const char op[]) {
 #define SHOULD_REGISTER_OP(op) ShouldRegisterOp(op)
 
 const char kNecessaryOpKernelClasses[] = ","
+"BiasAddOp<CPUDevice, float>,"
 "BinaryOp< CPUDevice, functor::add<float>>,"
 "BinaryOp< CPUDevice, functor::add<int32>>,"
 "ConcatOp<CPUDevice, ::tensorflow::int32>,"
 "ConcatOp<CPUDevice, float>,"
+"ConstOp<CPUDevice, float>,"  // Not sure if this is right.
 "ConstantOp,"
 "Conv2DOp<CPUDevice, float>,"
 "BinaryOp< CPUDevice, functor::div<float>>,"
