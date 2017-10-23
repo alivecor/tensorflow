37bcc8a3e18fd28427aed98211de6ae8663a8e91
Daniel Treiman <dtreiman@alivecor.com>
2017-10-23 13:48:14 -0700

    Added relu op.

--- tensorflow/core/util/version_info.cc
+++ tensorflow/core/util/version_info.cc
@@ -1,2 +1,2 @@
-const char* tf_git_version() {return "0.12.1-5-gb5e5120c5-dirty";}
+const char* tf_git_version() {return "0.12.1-25-g5933e912b-dirty";}
 const char* tf_compiler_version() {return __VERSION__;}
